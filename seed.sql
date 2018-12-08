da95fb05fce427d6e1dacb522a55937b0e79e2ab
laurenalyn <laurenalynwilkerson@gmail.com>
2018-12-07 19:54:12 -0500

    updates moods and users tables

--- seed.sql
+++ seed.sql
@@ -11,36 +11,31 @@ insert into users (firstname, lastname, email, username, pwhash) values ('Jessy'
 
 -- Data for moods table
 --=====================
-insert into moods (mood, description, color) values (1, 'Sad/Depressed', 'black' );
-insert into moods (mood, description, color) values (2, 'On Edge/Anxious', 'brown' );
-insert into moods (mood, description, color) values (3, 'High Energy/Angry', 'red' );
-insert into moods (mood, description, color) values (4, 'Unsettled/Surprised', 'orange' );
-insert into moods (mood, description, color) values (5, 'Excited/Adventurous', 'orange yellow' );
-insert into moods (mood, description, color) values (6, 'Create/Imaginative', 'yellow' );
-insert into moods (mood, description, color) values (7, 'Calm/Comfortable', 'green' );
-insert into moods (mood, description, color) values (8, 'Contentment/Peace', 'light blue' );
-insert into moods (mood, description, color) values (9, 'Relaxed/Lovable', 'indigo' );
-insert into moods (mood, description, color) values (10, 'Romantic/Sensual', 'violet' );
+insert into moods (mood, description, color) values (1, 'Happy', 'green' );
+insert into moods (mood, description, color) values (2, 'Normal', 'yellow' );
+insert into moods (mood, description, color) values (3, 'Sad', 'blue' );
+insert into moods (mood, description, color) values (4, 'Surprised', 'orange' );
+
 
 -- Data for entries table
 --=======================
-insert into entries (date, user_id, current_mood) values ('20180101', 1, 8);
-insert into entries (date, user_id, current_mood) values ('20180102', 1,  3);
-insert into entries (date, user_id, current_mood) values ('20180103', 1, 7);
-insert into entries (date, user_id, current_mood) values ('20180104', 1, 10);
-insert into entries (date, user_id, current_mood) values ('20180101', 2,  2);
-insert into entries (date, user_id, current_mood) values ('20180102', 2,  5);
-insert into entries (date, user_id, current_mood) values ('20180103', 2,  6);
-insert into entries (date, user_id, current_mood) values ('20180104', 2,  8);
-insert into entries (date, user_id, current_mood) values ('20180101', 3,  8);
-insert into entries (date, user_id, current_mood) values ('20180102', 3,  9);
-insert into entries (date, user_id, current_mood) values ('20180103', 3,  5);
-insert into entries (date, user_id, current_mood) values ('20180104', 3,  6);
-insert into entries (date, user_id, current_mood) values ('20180101', 4,  7);
-insert into entries (date, user_id, current_mood) values ('20180102', 4,  7);
-insert into entries (date, user_id, current_mood) values ('20180103', 4,  9);
-insert into entries (date, user_id, current_mood) values ('20180104', 4,  10);
-insert into entries (date, user_id, current_mood) values ('20180101', 5,  7);
-insert into entries (date, user_id, current_mood) values ('20180102', 5,  2);
-insert into entries (date, user_id, current_mood) values ('20180103', 5,  1);
-insert into entries (date, user_id, current_mood) values ('20180104', 5,  8);
+insert into entries (date, user_id, current_mood) values ('20180101', 1, 2);
+insert into entries (date, user_id, current_mood) values ('20180102', 1, 2);
+insert into entries (date, user_id, current_mood) values ('20180103', 1, 1);
+insert into entries (date, user_id, current_mood) values ('20180104', 1, 1);
+insert into entries (date, user_id, current_mood) values ('20180101', 2, 2);
+insert into entries (date, user_id, current_mood) values ('20180102', 2, 1);
+insert into entries (date, user_id, current_mood) values ('20180103', 2, 4);
+insert into entries (date, user_id, current_mood) values ('20180104', 2, 3);
+insert into entries (date, user_id, current_mood) values ('20180101', 3, 1);
+insert into entries (date, user_id, current_mood) values ('20180102', 3, 1);
+insert into entries (date, user_id, current_mood) values ('20180103', 3, 2);
+insert into entries (date, user_id, current_mood) values ('20180104', 3, 1);
+insert into entries (date, user_id, current_mood) values ('20180101', 4, 4);
+insert into entries (date, user_id, current_mood) values ('20180102', 4, 1);
+insert into entries (date, user_id, current_mood) values ('20180103', 4, 1);
+insert into entries (date, user_id, current_mood) values ('20180104', 4, 2);
+insert into entries (date, user_id, current_mood) values ('20180101', 5, 3);
+insert into entries (date, user_id, current_mood) values ('20180102', 5, 3);
+insert into entries (date, user_id, current_mood) values ('20180103', 5, 2);
+insert into entries (date, user_id, current_mood) values ('20180104', 5, 1);
